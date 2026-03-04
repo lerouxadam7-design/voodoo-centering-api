8eb0af820b25724da194114404087be8714bc4cc
lerouxadam7-design <lerouxadam7@gmail.com>
2026-03-04 11:16:44 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI, File, UploadFile
 import numpy as np
 import cv2
-from professional_centering import VoodooRawEngine
+from professional_centering import VoodooRawEngine, VoodooCornerCloseupEngine
 
 app = FastAPI()
 
-engine = VoodooRawEngine()
+raw_engine = VoodooRawEngine()
+corner_engine = VoodooCornerCloseupEngine()
+
 
 @app.post("/analyze")
 async def analyze(file: UploadFile = File(...)):
@@ -17,4 +19,17 @@ async def analyze(file: UploadFile = File(...)):
     if image is None:
         return {"error": "Invalid image"}
 
-    return engine.analyze_array(image)
+    return raw_engine.analyze_array(image)
+
+
+@app.post("/analyze_corner")
+async def analyze_corner(file: UploadFile = File(...)):
+
+    contents = await file.read()
+    nparr = np.frombuffer(contents, np.uint8)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    if image is None:
+        return {"error": "Invalid image"}
+
+    return corner_engine.analyze_array(image)
